7c750e7da0e96c53b706af0c5d610ffb76564cc1
Starcev Daniil <stdanmark@mail.ru>
2024-06-10 06:28:39 +0300

    first part of project

---
+++ main.py
@@ -0,0 +1,16 @@
+from Settings import COUNT
+from src.utils import load_json, get_executed_operations, sort_operations, get_last_operations, convert_date, \
+    convert_payment_info
+
+
+def main():
+    operations = load_json(path)
+    executed_operations = get_executed_operations(operations)
+    sorted_operations = sort_operations(executed_operations)
+    last_operations = get_last_operations(sorted_operations, COUNT)
+    for operation in last_operations:
+        converted_date = convert_date(operation["date"])
+        converted_payment_from = convert_payment_info(operation["from"])
+        converted_payment_to = convert_payment_info(operation["to"])
+        result = f""
+        print(result)
---
+++ src/utils.py
@@ -0,0 +1,38 @@
+import json
+from pathlib import Path
+
+
+def load_json(path):
+    """
+    Загружает json файл
+    """
+    with open(path, encoding="utf-8") as file:
+        return json.load(file)
+
+def get_executed_operations(operations):
+    """
+    Получает Executed операцию
+    """
+    for operation in operations:
+        if operation["state"] == "EXECUTED":
+            return operation
+
+
+def sort_operations(executed_operations):
+    """
+    Сортирует операции по дате 
+    """
+    for operation in operations:
+        if operation["date"] >
+
+
+def get_last_operations(sorted_operations, COUNT):
+    return operation[:COUNT]
+
+
+def convert_date(date):
+    datetime.fromisoformat(last_operations['date'])
+
+
+def convert_payment_info(payment_info):
+    if payment_info.startswith("Счет"):
